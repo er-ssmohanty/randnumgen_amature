a5ca2960fea5ca43d2adb751c3de6a6f553069f4
er-ssmohanty <pie2001@protonmail.com>
2022-11-04 11:12:32 +0530

    curr done

--- scripts/robust_randgen.py
+++ scripts/robust_randgen.py
@@ -1,34 +1,33 @@
 #! /usr/bin/python3
 def main():
-    import numpy as np
-    seed=float(input("Enter any real rational number: "))
-    howmany=int(input("Enter number of iterations: "))
-    randnum_list,pool_total=randnum_gen(seed,howmany)
-    # for i in range(len(pool_total)):
-	    # if pool_total[i]>1:
-		    # print("{}: {}".format(i,pool_total[i]))
+	import numpy as np
+	seed=float(input("Enter any real rational number: "))
+	howmany=int(input("Enter number of iterations: "))
+	randnum_list,pool_total=randnum_gen(seed,howmany)
+	# for i in range(len(pool_total))
+		# if pool_total[i]
+			# print("{}: {}".format(i,pool_total[i]))
+import numpy as np
 
 def randnum_gen(seed,maxcount,outputype="int",toprint=False):
-    import numpy as np
-    real_seed=(abs(seed**0.01)*1000000)%1000000
-    cnt=0
-    next_var=real_seed
-    maxval=1000
-    pool_list=[]
-    pool_freq=np.zeros(maxval)
-
-    while cnt<maxcount:
-        cnt+=1
-        next_var=(((((next_var+3.141592653589793)**2)+((next_var-3.141592653589793)\
-            **2)+(next_var**2))/3)*maxval)%maxval
-    if toprint:
-        print(int(next_var))
-    if outputype=="int":
-        pool_list.append(int(next_var))
-    else:
-        pool_list.append(next_var)
-    pool_freq[int(next_var)]+=1
-    return pool_list, pool_freq
+	real_seed=(abs(seed**0.01)*1000000)%1000000
+	cnt=0
+	next_var=real_seed
+	maxval=1000
+	pool_list=[]
+	pool_freq=np.zeros(maxval)
+	while cnt<maxcount:
+		cnt+=1
+		next_var=(((((next_var+3.141592653589793)**2)+((next_var-3.141592653589793)\
+            	**2)+(next_var**2))/3)*maxval)%maxval
+		if toprint:
+			print(int(next_var))
+		if outputype=="int":
+        		pool_list.append(int(next_var))
+		else:
+			pool_list.append(next_var)
+		pool_freq[int(next_var)]+=1
+	return pool_list, pool_freq
 
 if __name__ == "__main__":
-    main()
+	main()
--- scripts/test.py
+++ scripts/test.py
@@ -3,8 +3,12 @@ import robust_randgen as rr
 rr.randnum_gen(100,10)
 seeds=[92367628,45,0.0000341,0,-0.0000826,-88,-28787862]
 for i in seeds:
-    thelist,unnec=rr.randnum_gen(i,10)
-    with open("../results/{}_10_result.txt".format(i), "w") as f:
-        for s in thelist:
-            f.write(str(s) +"\n")
+	thelist,unnec=rr.randnum_gen(i,10)
+	#print(thelist)
+	#break
+	with open("../results/{}_10_result.txt".format(i), "a") as f:
+		#print(type(thelist))
+		#break
+		for s in thelist:
+			f.write(str(s) +"\n")
 
